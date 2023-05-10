9716e05b71b0798c676ac117c906f445338d95c9
ABHISHEK SPOT <113419871+AbhishekSpotOp@users.noreply.github.com>
2023-05-10 20:53:11 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-gray-800">
       <div className="max-w-6xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
         <p className="text-center text-gray-400">
-          &copy; Coded By Devansh. All rights reserved.
+          &copy; Coded By Abhishek. All rights reserved.
         </p>
       </div>
     </footer>
